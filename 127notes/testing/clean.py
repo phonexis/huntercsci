6d195c643b6b36dc961e0fcbbb4fd76db119461c
ef1301 <fangemily60@gmail.com>
2018-11-12 08:08:34 -0500

    punct

--- 127notes/testing/clean.py
+++ 127notes/testing/clean.py
@@ -8,31 +8,31 @@ def build_word_counts(string):
 def testing(filename):
     f = open(filename)
     s = f.read()
+    print(s)
     cleaned = clean(s)
+    f.close()
     return build_word_counts(cleaned)
 
 
-newfilename = "//home/fang/fall-2018-127/classcode/dictionaries/macbeth.txt"
+
+newfilename = "/home/fang/fall-2018-127/classcode/dictionaries/macbeth.txt"
 n = open(newfilename)
-o = n.read()
+o = n.readlines()
 
 def clean(s):
     result = ''
+    punct = "',:!?."
     for letter in s:
         if letter in "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ":
             result = result + letter.lower()
         else:
             result = result + " "
     for letter in result:
-        if letter == "'":
-            result = result + s.replace("'",'')
-        elif letter == ",":
-            result = result + s.replace(",",'')
-        elif letter == ":":
-            result = result + s.replace(":",'')
+        if letter in punct:
+            result = result + s.replace(punct,'')
         else:
             continue
     return result
 
 print(testing("/home/fang/fall-2018-127/classcode/dictionaries/macbeth.txt"))
-#print(testing("/home/fang/fall-2018-127/classcode/dictionaries/moby.txt"))
+##print(testing("/home/fang/fall-2018-127/classcode/dictionaries/moby.txt"))
