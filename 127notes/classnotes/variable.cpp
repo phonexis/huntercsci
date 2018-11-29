6b79e35ecdb32a44150603334bd6d8ccaff1b99d
ef1301 <fangemily60@gmail.com>
2018-11-29 08:56:51 -0500

    C++ intro

--- 127notes/classnotes/variable.cpp
+++ 127notes/classnotes/variable.cpp
@@ -2,7 +2,17 @@ int main()
 {
   int i;
   int some_variable = 10 , another_variable;
+  double d /* decimal point #*/
+
   i = 20;
   i = i +some_variable;
+  d = i;
+  
+  i = i / 4.2;
+  std::cout << "i=" << i << "\n";
+  /* this would return 7 because it's returning an int, not a float*/
+
+  d = d/4.2
+    std::cout << "d=" << d << "\n";
   return 0
 }
