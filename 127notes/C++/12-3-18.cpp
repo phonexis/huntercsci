2f4a7b0b38b7839f370de0cb42ca59e445236c90
ef1301 <fangemily60@gmail.com>
2018-12-03 08:32:04 -0500

    monday

---
+++ 127notes/C++/12-3-18.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdlibs>
+#include <ctime>
+
+int main(){
+  //establish the computers number
+  srand(time(NULL));
+  int answer = rand()%100;
+  /*
+  std::cout << answer << "\n";
+  answer = rand()%100;
+  std::cout << answer << "\n";
+  */
+  //repeat until the answer is guess
+  // get a guess from the user
+  // see if the guess is too low/high/just right
+  return 0;
+}
+/*pseudorandom- appears to be random so it has an aspect of fairness
+
+ */
--- 127notes/C++/hw_01.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main(){
-  int i = 1;
-  while(i<20){
-    std::cout<<"i="<<i<<std::endl;
-    i = i+1;
-  }
-}
