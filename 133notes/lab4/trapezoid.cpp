ea965cf8afa082be09269ad1ff82dc59f178f69b
ef1301 <fangemily60@gmail.com>
2019-02-21 08:08:46 -0500

    trapezoid done

--- 133notes/lab4/trapezoid.cpp
+++ 133notes/lab4/trapezoid.cpp
@@ -2,12 +2,46 @@
 
 std::string trapezoid(int width, int height){
   std::string box;
+  int left = 0, right = width-1;
   if (height > width/2){
     return "Impossible shape";
   }
+  else {
+    for (int rows=0;rows<height;rows++){
+      if (rows == 0){
+	for (int i = 0; i<width;i++){
+	  box += "*";
+	}
+	box+= "\n";
+      }
+      else{
+	for (int i = 0; i<width; i++){
+	  if (i <= left){
+	    box += " ";
+	  }
+	  else if (i >= right){
+	    box += " ";
+	  }
+	  else {
+	    box += "*";
+	  }
+	}
+	box += "\n";
+	left += 1;
+	right -= 1;
+      }
+    }
+  }
+  return box;
 }
 
 int main(){
+  int width, height;
+  std::cout << "Input trapezoid width: ";
+  std::cin >> width;
+  std::cout << "Input trapezoid height: ";
+  std::cin >> height;
 
+  std::cout << trapezoid(width,height);
   return 0;
 }
