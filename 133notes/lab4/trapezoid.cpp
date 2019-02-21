01e0ad79d0cca1bbe93b3dbe397b4179b675d840
ef1301 <fangemily60@gmail.com>
2019-02-21 00:10:33 -0500

    not done with task f

---
+++ 133notes/lab4/trapezoid.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+std::string trapezoid(int width, int height){
+  std::string box;
+  if (height > width/2){
+    return "Impossible shape";
+  }
+}
+
+int main(){
+
+  return 0;
+}
