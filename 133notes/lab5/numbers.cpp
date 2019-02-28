1e138e7131ec7baf08e9d03f92099e641ab772bb
ef1301 <fangemily60@gmail.com>
2019-02-28 09:55:02 -0500

    task b done

--- 133notes/lab5/numbers.cpp
+++ 133notes/lab5/numbers.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
+//TASK A
 bool isDivisible(int n, int d){
   if (n%d==0){
     return true;
@@ -9,12 +10,47 @@ bool isDivisible(int n, int d){
   }
 }
 
+//TASK B
+bool isPrime(int n){
+  int count=0;
+  for(int i=2;i<n;i++){
+    if(count==1){
+      return false;
+    }
+    else if(n%i==0){
+      count+=1;
+    }
+  }
+  return true;
+}
+
+//TASK C
+/*
+int nextPrime(int n){
+  int next = n+1;
+  if(isPrime(next)==0){
+    next+=1;
+  }
+  else {
+    return next;
+  }
+}
+*/
+
+//TASK D
+int countPrimes(int a, int b){
+
+}
+
+//TASK E
+bool isTwinPrime(int n){
+
+}
+
 int main(){
-  int a, b;
-  std::cout << "First number: ";
-  std::cin >> a;
-  std::cout << "Second number: ";
-  std::cin >> b;
-  std::cout << isDivisible(a,b) << std::endl;
+  std::cout << isDivisible(6,2) << std::endl;
+  std::cout << isPrime(5) << std::endl;
+  std::cout << isPrime(49) << std::endl;
+  //  std::cout << nextPrime(14) << std::endl;
   return 0;
 }
