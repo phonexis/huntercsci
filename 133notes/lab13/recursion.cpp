d098b1b772ce23e1472252dcda176fe8f4a8f0c4
ef1301 <fangemily60@gmail.com>
2019-05-08 13:20:36 -0400

    task a,b,c

--- 133notes/lab13/recursion.cpp
+++ 133notes/lab13/recursion.cpp
@@ -14,18 +14,58 @@ void printRange(int left, int right){
 int sumRange(int left, int right){
   int sum=0;
   if(right>=left){
-    sum+=left;
-    sumRange(left+1,right);
+    sum=left+sumRange(left+1,right);
+  }
+  return sum;    
+}
+
+int sumArrayInRange(int *arr, int left, int right){
+  int sum=0;
+  if(right>=left){
+    sum=arr[left]+sumArrayInRange(arr,left+1,right);
   }
   return sum;
 }
 
 int sumArray(int *arr, int size){
-  return 0;
+  int sum = 0;
+  int index = 0;
+  sum=arr[index]+sumArrayInRange(arr, index+1, size-1);
+  return sum;
+}
+
+bool isAlphanumeric(std::string s){
+  if(s.length()==0){
+    return true;
+  }
+  else if(isalpha(s) || isdigit(s)){
+    return true;
+  }
+  return false;
 }
 
 int main(){
-  printRange(-2,10);
-  std::cout << sumRange(1,3) << " " << sumRange(-2,10);
+  //printRange(-2,10);
+  //std::cout << sumRange(1,3) << " "; //<< sumRange(-2,10);
+  /*int size = 10;
+  int *arr = new int[size]; // allocate array dynamically
+  arr[0] = 12;
+  arr[1] = 17;
+  arr[2] = -5;
+  arr[3] = 3;
+  arr[4] = 7;
+  arr[5] = -15;
+  arr[6] = 27;
+  arr[7] = 5;
+  arr[8] = 13;
+  arr[9] = -21;
+
+  int sum1 = sumArray(arr, size); // Add all elements
+  std::cout << "Sum is " << sum1 << std::endl;  // Sum is 43
+    
+  int sum2 = sumArray(arr, 5); // Add up first five elements
+  std::cout << "Sum is " << sum2 << std::endl;  // Sum is 34
+
+  delete[] arr;         // deallocate it */
   return 0;
 }
