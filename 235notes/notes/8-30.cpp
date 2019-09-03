83ddafd7b7ee2dd0ce478038c1ca0603e8188fed
Emily Fang <fangemily60@gmail.com>
2019-09-03 11:15:31 -0400

    first day

---
+++ 235notes/notes/8-30.cpp
@@ -0,0 +1,22 @@
+//Abstraction and OOP(Object Oriented Programming)
+/*
+Encapsualtion-objects combine data and operations:
+class SomeClass{
+//access specifier which can be private, public, or protected
+//data members and member functions
+};
+
+A user-defied data type that bundles  together data and operations on the data.
+
+Information Hiding- objects hide inner details:
+From now on, everytime you design a class, you will be separating the interface with implementation.
+
+Data members:the attributes/data
+Member funcitons: the operations/actions availabe on the data
+ -Mutator functions: modify the data members
+ -Accessor fuctnions: retrieve the value of data members (using const)
+ -Constructor
+ -Destructor
+
+Class:type (like int)
+Object:instance of the class
