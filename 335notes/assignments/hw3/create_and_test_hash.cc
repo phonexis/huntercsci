c08875af0b5d4929a63c6358abe87b82af8409f8
Emily Fang <40726681+ef1301@users.noreply.github.com>
2020-03-27 17:24:15 -0400

    hw3

---
+++ 335notes/assignments/hw3/create_and_test_hash.cc
@@ -0,0 +1,58 @@
+// YOUR NAME.
+
+#include "quadratic_probing.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+
+template <typename HashTableType>
+void TestFunctionForHashTable(HashTableType &hash_table, const string &words_filename, const string &query_filename) {
+  cout << "TestFunctionForHashTables..." << endl;
+  cout << "Words filename: " << words_filename << endl;
+  cout << "Query filename: " << query_filename << endl;
+  hash_table.MakeEmpty();  
+  //..Insert your own code
+
+  cout << "Collisions: " << endl;
+  cout << "Number of items: " << endl;
+  cout << "Size of hash table: " << endl;
+  cout << "Load factor: " << endl;
+  cout << "Avg. number of collisions: " << endl;
+}
+
+int testFunctionWrapper(int argument_count, char **argument_list) {
+  const string words_filename(argument_list[1]);
+  const string query_filename(argument_list[2]);
+  const string param_flag(argument_list[3]);
+
+  if (param_flag == "linear") {
+    // HashTableLinear<string> linear_probing_table;
+    // TestFunctionForHashTable(linear_probing_table, words_filename, query_filename);    
+  } else if (param_flag == "quadratic") {
+    HashTable<string> quadratic_probing_table;
+    TestFunctionForHashTable(quadratic_probing_table, words_filename, query_filename);    
+  } else if (param_flag == "double") {
+    // HashTableDouble<string> double_probing_table;
+    // TestFunctionForHashTable(double_probing_table, words_filename, query_filename);    
+  } else {
+    cout << "Unknown tree type " << param_flag << " (User should provide linear, quadratic, or double)" << endl;
+  }
+  return 0;
+}
+
+// Sample main for program create_and_test_hash.
+// WE WILL NOT USE YOUR MAIN IN TESTING. DO NOT CODE FUNCTIONALITY INTO THE MAIN.
+// WE WILL DIRECTLY CALL testFunctionWrapper. ALL FUNCTIONALITY SHOULD BE THERE.
+// This main is only here for your own testing purposes.
+int main(int argc, char **argv) {
+
+  if (argc != 4) {
+    cout << "Usage: " << argv[0] << " <wordsfilename> <queryfilename> <flag>" << endl;
+    return 0;
+  }
+  
+  testFunctionWrapper(argc, argv);
+  return 0;
+}
