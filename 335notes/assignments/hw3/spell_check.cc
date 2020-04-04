95354d99b2192efd8251cb23a22e589c155480fd
Emily Fang <fangemily60@gmail.com>
2020-04-04 16:09:29 -0400

    Q1 done

--- 335notes/assignments/hw3/spell_check.cc
+++ 335notes/assignments/hw3/spell_check.cc
@@ -6,19 +6,59 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <cctype>
+#include <algorithm>
 using namespace std;
 
-/*string taskA(string s) {
-
+string clean(string s) {
+  int index = 0;
+  for(char &c: s) {
+    if(c != 39 && c != 45 && ispunct(c)) {
+      s.erase(index, 1);
+    }
+    if(isalpha(c) && c >= 'A' && c <= 'Z') {
+      c += 32;
+    }
+    index ++;
+  }
+  return s;
 }
 
-string taskB(string s) {
-
+vector<string> taskA(string s) {
+  vector<string> possibilities;
+  size_t i = 0;
+  while (i <= s.length()) {
+    string temp = s;
+    for (char c = 'a'; c <= 'z'; ++c) {
+      possibilities.push_back(s.substr(0,i) + c + s.substr(i,s.length( )));
+    }
+    i++;
+  }
+  return possibilities;
 }
 
-string taskC(string s) {
+vector<string> taskB(string s) {
+  vector<string> possibilities;
+  size_t i = 0;
+  while (i < s.length()) {
+    string temp = s;
+    possibilities.push_back(temp.erase(i, 1));
+    i++;
+  }
+  return possibilities;
+}
 
-}*/
+vector<string> taskC(string s) {
+  vector<string> possibilities;
+  size_t i = 0;
+  while (i < s.length()) {
+    string temp = s;
+    std::swap(temp[i], temp[i+1]);
+    possibilities.push_back(temp);
+    i++;
+  }
+  return possibilities;
+}
 
 int testSpellingWrapper(int argument_count, char** argument_list) {
   const string document_filename(argument_list[1]);
@@ -36,21 +76,42 @@ int testSpellingWrapper(int argument_count, char** argument_list) {
   }
   dictionary_file.close();
 
-
+  int found = 0;
   ifstream document_file;
   document_file.open(document_filename);
   
   while(getline(document_file, input)) {
     std::stringstream ss(input);
     while(ss >> word) {
-      if(dictionary.Contains(word, collisions)) {
-	std::cout << word << " is CORRECT" << std::endl;
+      string cleaned_word =  clean(word);
+      if(dictionary.Contains(cleaned_word, collisions)) {
+	found ++;
+	std::cout << cleaned_word << " is CORRECT" << std::endl;
       }
       else {
-	//std::cout << input << " is INCORRECT" << std::endl;
-	//std::cout << "** " << input << " -> " << " ** case A" << std::endl;
-	//std::cout << "** " << input << " -> " << " ** case B" << std::endl;
-	//std::cout << "** " << input << " -> " << " ** case C" << std::endl;
+	std::cout << cleaned_word << " is INCORRECT" << std::endl;
+
+	for(auto possibility: taskA(cleaned_word)) {
+	  if(dictionary.Contains(possibility, collisions)) {
+	    found ++;
+	    std::cout << "** " << cleaned_word << " -> " << possibility << " ** case A" << std::endl;
+	  }
+	}
+	
+	for(auto possibility: taskB(cleaned_word)) {
+	  if(dictionary.Contains(possibility, collisions)) {
+	    found ++;
+	    std::cout << "** " << cleaned_word << " -> " << possibility << " ** case B" << std::endl;
+	  }
+	}
+
+	for(auto possibility: taskC(cleaned_word)) {
+	  if(dictionary.Contains(possibility, collisions)) {
+	    found ++;
+	    std::cout << "** " << cleaned_word << " -> " << possibility << " ** case C" << std::endl;
+	  }
+	}
+	
       }
     }
   }
@@ -58,7 +119,7 @@ int testSpellingWrapper(int argument_count, char** argument_list) {
   document_file.close();
 
   // Call functions implementing the assignment requirements.
-  return 0;
+  return found;
 }
 
 // Sample main for program spell_check.
